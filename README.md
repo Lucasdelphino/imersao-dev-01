57c27e610d9a8e5bb0cc870a07db13ce49e9739c
Lucasdelphino <80285529+Lucasdelphino@users.noreply.github.com>
2022-03-16 17:32:13 -0300

    imesao-dev-aula1

--- README.md
+++
@@ -1,2 +0,0 @@
-# imersao-dec-01
- 
---
+++ funcoes.js
@@ -0,0 +1,13 @@
+var nome = "Lucas"
+
+var notaDoPrimeiroBimestre = 10
+var notaDoSegundoBimestre = 10
+var notaDoTerceiroBimestre = 10
+var notaDoQuartoBimestre = 10
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
